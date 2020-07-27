15704cb22a18f3a835010324005cd1fb99fc2427
Akimasa Liu <liumingzheng8@outlook.com>
2020-07-27 23:19:18 +0900

    wrote more coment in hooks.py

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -1,3 +1,8 @@
 #Please make file in [your git directory]/.git/hooks/post-receive 
 #and do not add extension after the file name.
-
+#I wrote the script in the "post-recieve" in below.
+"""
+echo "start pushing"
+cd ../../../hooks
+hooks.sh
+"""
